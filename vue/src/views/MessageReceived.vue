b0d6b24c3a84ec4afcb063ae43e7759b5fd09e54
yandong55 <yandong55@vt.edu.com>
2022-10-21 11:25:52 -0400

    Update workout partner and group activity

--- vue/src/views/MessageReceived.vue
+++ vue/src/views/MessageReceived.vue
@@ -1,11 +1,24 @@
 <template>
 Message Received
-</template>
+
 
 <script>
 
 </script>
 
-<style scoped>
 
+<style scoped>
+.user-scroll-panel {
+  box-sizing: border-box;
+  /*background-color: #2c3e50;*/
+  min-height: 100vh;
+  padding-top: 3rem;
+}
+
+main {
+  pading: 0 2rem;
+  max-width: 640px;
+  margin: 0 auto;
+
+}
 </style>
