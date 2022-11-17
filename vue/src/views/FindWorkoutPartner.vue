56552eeb3ec3fff1daa197204993b7066c6c16e4
Alisa Khuu <alisakhuu@gmail.com>
2022-11-17 01:33:12 -0500

    cleanup and adding missing file

--- vue/src/views/FindWorkoutPartner.vue
+++ vue/src/views/FindWorkoutPartner.vue
@@ -1,12 +1,22 @@
 <template>
-Message Received
+  <div class="user-scroll-panel">
+    <div style="margin-bottom: 20px">
+      <el-input style="width: 260px; margin-right: 10px" v-model="sex" placeholder="Sex" clearable></el-input>
+      <el-input style="width: 260px; margin-right: 10px" v-model="ageLower" placeholder="Age from" clearable></el-input>
+      <el-input style="width: 260px; margin-right: 10px" v-model="ageUpper" placeholder="Age to" clearable></el-input>
+      <el-button type="primary" @click="match"><el-icon style="margin-right: 3px"><Search /></el-icon> Match</el-button>
+    </div>
+
+    <main>
+      <PostPartner v-for="(anime, i) in anime_list" :key="i" :anime="anime"/>
+    </main>
+  </div>
 </template>
 
 
 
 
 <script>
-
 import PostPartner from "./PostPartner.vue";
 import { Search } from '@element-plus/icons-vue'
 import {reactive, ref} from "vue";
@@ -74,8 +84,68 @@ export default {
       //         "Alex Coolkid, 23 Blacksburg, VA",
       //   });
       // }
-      const anime = [];
 
+      const anime_titles = [
+        "User 1",
+        "User 2",
+        "User 3",
+        "User 4",
+        "User 5",
+        "User 6",
+        "User 7",
+        "User 8",
+        "User 9",
+        "User 10",
+      ];
+      const anime_ages = [
+        "24",
+        "36",
+        "25",
+        "28",
+        "30",
+        "32",
+        "23",
+        "40",
+        "27",
+        "49",
+      ];
+      const anime_address = [
+        "Blacksburg, VA, 22030",
+        "College Park, MD, 15641",
+        "Falls Church, VA, 45268",
+        "Washington, D.C.",
+        "Blacksburg, VA",
+        "College Park, MD",
+        "Falls Church, VA",
+        "Washington, D.C.",
+        "Blacksburg, VA",
+        "College Park, MD",
+      ];
+      const anime_imgSrc = [
+        "user1.jpg",
+        "user2.jpg",
+        "user3.jpg",
+        "user4.jpg",
+        "user5.jpg",
+        "user6.jpg",
+        "user7.jpg",
+        "WinderIcon.jpg",
+        "WinderIcon.jpg",
+        "WinderIcon.jpg",
+      ];
+      const anime_ids = [
+        1001,
+        1002,
+        1003,
+        1004,
+        1005,
+        1006,
+        1007,
+        1008,
+        1009,
+        1010,
+      ];
+      const anime = [];
       // for (let i = 0; i < anime_titles.length; i++) {
       //   // let userIndex = Math.floor(Math.random() * anime.title.length);
       //   anime.push({
@@ -94,6 +164,7 @@ export default {
       if (state.tableData) {
         this.match()
       for (let i = 0; i < 3; i++ ) {
+
           anime.push({
             // title: state.tableData[i].id,
             // description: state.tableData[i].username
@@ -105,7 +176,6 @@ export default {
             id: state.tableData[i].id
           })
         }
-
       }
 
       return anime;
@@ -150,30 +220,26 @@ export default {
       })
     }
 
-
-
   },
   mounted() {
     this.anime_list = this.getAnime();
     window.addEventListener("scroll", this.handleScroll);
   },
-};
-
+}
 </script>
 
 
 <style scoped>
 .user-scroll-panel {
   box-sizing: border-box;
-  /*background-color: #2c3e50;*/
-  min-height: 100vh;
-  padding-top: 3rem;
+  min-height: 120vh;
 }
 
 main {
-  pading: 0 2rem;
-  max-width: 640px;
-  margin: 0 auto;
-
+  background-color: lightgray;
+  width: 565px;
+  /*pading: 2rem 2rem;*/
+  margin: 2rem auto;
+  border-radius: 2rem;
 }
 </style>
